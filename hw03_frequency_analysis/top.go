f9a9162923a7022e376261c299f5a96f088bd826
Dmitriy <dimonk33@users.noreply.github.com>
2022-12-27 16:54:40 +0300

    Hw03 frequency analysis (#4)
    * 1) Простая версия
    
    * 1) Звездочка
    
    * 1) Оптимизация кода
    
    * 1) fix линтера
    
    * 1) оптимизация, исключение слайса
    
    Co-authored-by: kuzmindv <dmitrijj.kuzmin.v@vseinstrumenti.ru>

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,44 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"sort"
+	"strings"
+)
+
+func getKeysArr(arr map[string]int) []string {
+	keys := make([]string, 0, len(arr))
+	for key := range arr {
+		keys = append(keys, key)
+	}
+	return keys
+}
+
+func Top10(text string) []string {
+	wordMap := map[string]int{}
+	wordArr := strings.FieldsFunc(text, func(r rune) bool {
+		return strings.ContainsRune(" ,.:!?\n\t\"'`", r)
+	})
+	for _, word := range wordArr {
+		word = strings.ToLower(strings.TrimSpace(word))
+		if word == "" || word == "-" {
+			continue
+		}
+		wordMap[word]++
+	}
+
+	keysArr := getKeysArr(wordMap)
+
+	sort.Slice(keysArr, func(i, j int) bool {
+		if wordMap[keysArr[i]] == wordMap[keysArr[j]] {
+			return keysArr[i] < keysArr[j]
+		}
+		return wordMap[keysArr[i]] > wordMap[keysArr[j]]
+	})
+
+	sortWordCount := len(wordMap)
+	if sortWordCount > 10 {
+		sortWordCount = 10
+	}
+
+	return keysArr[:sortWordCount]
 }
