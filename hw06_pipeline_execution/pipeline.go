327afd5e476db1debb1e472fcb9d0d6a36f24632
Dmitriy <dimonk33@users.noreply.github.com>
2023-02-12 22:23:53 +0300

    1) реализация пайплайна (#7)
    * 1) реализация пайплайна
    
    * 1) добавлен тест
    
    * доработка
    
    * 1) fix линтер
    
    ---------
    
    Co-authored-by: kuzmindv <dmitrijj.kuzmin.v@vseinstrumenti.ru>

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -1,5 +1,10 @@
 package hw06pipelineexecution
 
+import (
+	"fmt"
+	"time"
+)
+
 type (
 	In  = <-chan interface{}
 	Out = In
@@ -9,6 +14,28 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	from := in
+	start := time.Now()
+	for i, stage := range stages {
+		inCh := make(Bi)
+
+		go func(id int, in_ In, out_ Bi, stage_ Stage) {
+			defer close(out_)
+			fmt.Printf("stage %d start %v\r\n", id, time.Since(start))
+			stCh := stage_(in_)
+			for val := range stCh {
+				select {
+				case <-done:
+					return
+				default:
+					out_ <- val
+				}
+			}
+			fmt.Printf("stage %d finished %v\r\n", id, time.Since(start))
+		}(i, from, inCh, stage)
+
+		from = inCh
+	}
+
+	return from
 }
