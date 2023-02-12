b5b4edbf1ff00d52539492574436cf9728ce3c1a
kuzmindv <dmitrijj.kuzmin.v@vseinstrumenti.ru>
2023-02-12 14:54:25 +0300

    доработка

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -1,6 +1,9 @@
 package hw06pipelineexecution
 
-import "sync"
+import (
+	"fmt"
+	"time"
+)
 
 type (
 	In  = <-chan interface{}
@@ -11,88 +14,28 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	mu := sync.RWMutex{}
-	out := make(Bi)
+	var from = in
+	start := time.Now()
+	for i, stage := range stages {
+		inCh := make(Bi)
 
-	id := 0
-	order := 0
-	for val := range in {
-		go stageThread(id, &order, &mu, stages, val, out, done)
-		id++
-	}
-
-	go func(_order *int) {
-		defer close(out)
-		exit := false
-		for !exit {
-			select {
-			case _, ok := <-done:
-				if !ok {
+		go func(id int, in_ In, out_ Bi, stage_ Stage) {
+			defer close(out_)
+			fmt.Printf("stage %d start %v\r\n", id, time.Since(start))
+			stCh := stage_(in_)
+			for val := range stCh {
+				select {
+				case <-done:
 					return
+				default:
+					out_ <- val
 				}
-			default:
-				mu.RLock()
-				exit = *_order >= id
-				mu.RUnlock()
 			}
-		}
-	}(&order)
+			fmt.Printf("stage %d finished %v\r\n", id, time.Since(start))
+		}(i, from, inCh, stage)
 
-	return out
-}
-
-func stageThread(
-	id int,
-	order *int,
-	mu *sync.RWMutex,
-	stages []Stage,
-	val interface{},
-	out chan<- interface{},
-	done In,
-) {
-	ch := make(Bi, 1)
-	ch <- val
-	close(ch)
-
-	var _out Out = ch
-	for _, st := range stages {
-		select {
-		case _, ok := <-done:
-			if !ok {
-				return
-			}
-		default:
-			_out = st(_out)
-		}
-	}
-	val = <-_out
-
-	exit := false
-	for !exit {
-		select {
-		case _, ok := <-done:
-			if !ok {
-				return
-			}
-		default:
-			mu.RLock()
-			exit = *order >= id
-			mu.RUnlock()
-		}
+		from = inCh
 	}
 
-	exit = false
-	for !exit {
-		select {
-		case _, ok := <-done:
-			if !ok {
-				return
-			}
-		case out <- val:
-			mu.Lock()
-			*order++
-			mu.Unlock()
-			return
-		}
-	}
+	return from
 }
