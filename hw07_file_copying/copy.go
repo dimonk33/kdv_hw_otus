3ebe6a348da0db261bb0c7899bacac1f799c4c55
Dmitriy <dimonk33@users.noreply.github.com>
2023-02-15 13:45:10 +0300

    1) утилита для копирования (#8)
    * 1) утилита для копирования
    
    * 1) форматирование
    
    * 1) форматирование
    
    * 1) тесты
    2) прогресс-бар
    
    * 1) правки после проверки
    
    * 1) правки после проверки
    
    ---------
    
    Co-authored-by: kuzmindv <dmitrijj.kuzmin.v@vseinstrumenti.ru>

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,14 +2,89 @@ package main
 
 import (
 	"errors"
+	"fmt"
+	"io"
+	"os"
+	"path"
+
+	"github.com/cheggaaa/pb/v3"
 )
 
 var (
+	ErrFileNotExist          = errors.New("file not exist")
+	ErrCreateFile            = errors.New("file create error")
 	ErrUnsupportedFile       = errors.New("unsupported file")
 	ErrOffsetExceedsFileSize = errors.New("offset exceeds file size")
+	ErrSeekFile              = errors.New("file seek error")
+	ErrCopyFile              = errors.New("file copy error")
+	ErrPathFiles             = errors.New("path for files error")
+	ErrParams                = errors.New("limit or offset error")
 )
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+	fromPath = path.Clean(fromPath)
+	toPath = path.Clean(toPath)
+
+	if fromPath == toPath {
+		return ErrPathFiles
+	}
+
+	if offset < 0 || limit < 0 {
+		return ErrParams
+	}
+
+	fileFrom, errFrom := os.Open(fromPath)
+	if errFrom != nil {
+		if os.IsNotExist(errFrom) {
+			return ErrFileNotExist
+		}
+		return errFrom
+	}
+	defer closeFile(fileFrom)
+
+	info, errSize := fileFrom.Stat()
+	fileSize := info.Size()
+	if errSize != nil {
+		return ErrUnsupportedFile
+	}
+
+	if offset >= fileSize {
+		return ErrOffsetExceedsFileSize
+	}
+
+	_ = os.Remove(toPath)
+	fileTo, errTo := os.Create(toPath)
+	if errTo != nil {
+		return ErrCreateFile
+	}
+	defer closeFile(fileTo)
+
+	copySize := fileSize - offset
+	if limit > 0 && limit < copySize {
+		copySize = limit
+	}
+	_, errSeek := fileFrom.Seek(offset, io.SeekStart)
+	if errSeek != nil {
+		return ErrSeekFile
+	}
+
+	bar := pb.Full.Start64(copySize)
+	barReader := bar.NewProxyReader(fileFrom)
+
+	_, errCopy := io.CopyN(fileTo, barReader, copySize)
+
+	bar.Finish()
+	fmt.Println("")
+
+	if errCopy != nil {
+		return ErrCopyFile
+	}
+
 	return nil
+}
+
+func closeFile(file *os.File) {
+	if err := file.Close(); err != nil {
+		fmt.Printf("%v", err)
+	}
 }
--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,46 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestCopy(t *testing.T) {
-	// Place your code here.
+	input := "testdata/input.txt"
+	output := t.TempDir() + "/out.txt"
+
+	t.Run("identical in and output files", func(t *testing.T) {
+		res := Copy(input, "testdata/../"+input, 100000, 0)
+		require.Error(t, res)
+	})
+
+	t.Run("offset less zero", func(t *testing.T) {
+		res := Copy(input, output, -1, 0)
+		require.Error(t, res)
+	})
+
+	t.Run("limit less zero", func(t *testing.T) {
+		res := Copy(input, output, 0, -1)
+		require.Error(t, res)
+	})
+
+	t.Run("offset over file size", func(t *testing.T) {
+		res := Copy(input, output, 100000, 0)
+		require.Error(t, res)
+	})
+
+	t.Run("limit over file size", func(t *testing.T) {
+		res := Copy(input, output, 0, 1000000000)
+		require.Nil(t, res)
+		require.FileExists(t, input)
+		require.FileExists(t, output)
+
+		inputStat, inputErr := os.Stat(input)
+		outputStat, outputErr := os.Stat(output)
+		require.Nil(t, inputErr)
+		require.Nil(t, outputErr)
+		require.Equal(t, inputStat.Size(), outputStat.Size())
+	})
 }
