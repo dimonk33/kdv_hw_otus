67c57dacd86e596d1360ec3029833708f463ca5a
kuzmindv <dmitrijj.kuzmin.v@vseinstrumenti.ru>
2023-03-22 01:14:00 +0300

    1) Откат тестов

--- hw11_telnet_client/test.sh
+++ hw11_telnet_client/test.sh
@@ -3,11 +3,11 @@ set -xeuo pipefail
 
 go build -o go-telnet
 
-(echo -e "Hello\nFrom\nNC\n" && cat 2>/dev/null) | "/c/Work/Utilities/Nmap/ncat.exe" -l localhost 4242 >/d/kuzmindv/temp/nc.out &
+(echo -e "Hello\nFrom\nNC\n" && cat 2>/dev/null) | nc -l localhost 4242 >/tmp/nc.out &
 NC_PID=$!
 
 sleep 1
-(echo -e "I\nam\nTELNET client\n" && cat 2>/dev/null) | ./go-telnet --timeout=5s localhost 4242 >/d/kuzmindv/temp/telnet.out &
+(echo -e "I\nam\nTELNET client\n" && cat 2>/dev/null) | ./go-telnet --timeout=5s localhost 4242 >/tmp/telnet.out &
 TL_PID=$!
 
 sleep 5
@@ -23,12 +23,12 @@ function fileEquals() {
 expected_nc_out='I
 am
 TELNET client'
-fileEquals /d/kuzmindv/temp/nc.out "${expected_nc_out}"
+fileEquals /tmp/nc.out "${expected_nc_out}"
 
 expected_telnet_out='Hello
 From
 NC'
-fileEquals /d/kuzmindv/temp/telnet.out "${expected_telnet_out}"
+fileEquals /tmp/telnet.out "${expected_telnet_out}"
 
 rm -f go-telnet
 echo "PASS"
